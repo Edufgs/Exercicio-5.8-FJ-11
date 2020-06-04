aae24a592c4293173b14337cff560a927af8577f
Eduardo Gonçalves da Silva <44234388+Edufgs@users.noreply.github.com>
2020-06-04 09:58:06 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package Main;
+
+import Conta.Conta;
+import java.util.Calendar;
+
+public class Main {
+    public static void main(String[] args){
+        //Conta pessoa = new Conta("Eduardo Gonçalves", "056.765.191-6", 1);
+           
+                int nContas = 0;
+        Conta pessoa1 = new Conta();
+        nContas = nContas + 1;
+        
+        pessoa1.setNome("Puta");
+        pessoa1.setCpf("056.165.754-05");
+        pessoa1.setIdentificador(nContas);
+        System.out.println(pessoa1.setData(30,3,2020) +"\n");
+        
+        System.out.println(pessoa1.getNome());
+        System.out.println(pessoa1.getCpf());
+        System.out.println(pessoa1.getIdentificador());
+        System.out.println(pessoa1.getDia() +"/"+ pessoa1.geMes() +"/"+ pessoa1.getAno());
+
+    }
+}
