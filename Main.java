dfb9ec1335d43b090c5eae97835d3f8e96315a85
Eduardo Gonçalves da Silva <44234388+Edufgs@users.noreply.github.com>
2020-06-12 10:14:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,22 @@
 package Main;
 
 import Conta.Conta;
-import java.util.Calendar;
+
 
 public class Main {
     public static void main(String[] args){
-          int nContas = 0;
-        Conta pessoa1 = new Conta();
-        nContas++;
-        //Conta pessoa = new Conta("Eduardo Gonçalves", "056.765.191-6", nContas);
-       
+        Conta pessoa = new Conta("Eduardo Gonçalves", "056.765.191-6", "25546-5", 500, 16, 6, 2000);
+           
+        //Conta pessoa1 = new Conta();
+        
+                
+        //pessoa1.setNome("Puta");
+        //pessoa1.setCpf("056.165.754-05");
         
         
-        pessoa1.setNome("Puta");
-        pessoa1.setCpf("056.165.754-05");
-        pessoa1.setIdentificador(nContas);
-        
-        System.out.println(pessoa1.setData(30,2,2020) +"\n");
-        
-        /*System.out.println(pessoa1.getNome());
-        System.out.println(pessoa1.getCpf());
-        System.out.println(pessoa1.getIdentificador());
-        System.out.println(pessoa1.getDia() +"/"+ pessoa1.geMes() +"/"+ pessoa1.getAno());*/
-
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getCpf());
+        System.out.println(pessoa.getIdentificador());
+        //System.out.println(pessoa.getDia() +"/"+ pessoa.getMes() +"/"+ pessoa.getAno());
     }
 }
